12959b46c75760f78c5821ffde20605ce851a560
NathanV <nathan.t.vik@gmail.com>
2021-09-28 16:25:36 -0400

    class 4

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
-from .views import BlogListView
-
+from .views import (
+    BlogCreateView,
+    BlogListView, 
+    BlogDetailView,
+    BlogUpdateView,
+    BlogDeleteView,
+    SignUpView,
+)
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
+    path('posts/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('posts/new/', BlogCreateView.as_view(), name='post_new'),
+    path('posts/<int:pk>/update/', BlogUpdateView.as_view(), name='post_update'),
+    path('posts/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+    path('signup', SignUpView.as_view(), name="signup")
 ]
