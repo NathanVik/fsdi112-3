4d49b420e905d9ec31c4a99bf1be8be5a89b06a4
NathanV <nathan.t.vik@gmail.com>
2021-10-02 12:22:40 -0400

    mixin for user/author verification on edits

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,11 @@ from django.views.generic.edit import (
     UpdateView,
     DeleteView
 )
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+)
 from django.urls import reverse_lazy
-from django.contrib.auth.forms import UserCreationForm
 
 from .models import Post
 
@@ -17,18 +20,26 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
 
-class BlogCreateView(CreateView):
+class BlogCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = "post_new.html"
     fields = ["title", "author", "body"]
 
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = "post_edit.html"
     fields = ["title", "body"]
 
-class BlogDeleteView(DeleteView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name="post_delete.html"
     success_url= reverse_lazy('home')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-un9pvyqhp(^v$-*_gylb-qw97-f4op*w_i&g$hzd5ltg%3q6%4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", ".herokuapp.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".herokuapp.com"]
 
 
 # Application definition
